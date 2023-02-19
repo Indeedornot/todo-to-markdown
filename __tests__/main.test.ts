1e177af2fdf1a1158c5ce7d1cd15a7509b140427
indeedornot <gravitybartek@gmail.com>
2023-02-19 19:03:33 +0100

    update test methods

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,62 +1,47 @@
-import * as process from 'process';
-import * as cp from 'child_process';
-import * as path from 'path';
 import * as fs from 'fs/promises';
 import {createSegment, getSegment, updateSegment} from '../src/segments';
 
-describe('Mock Test', () => {
-	test('Mock Test', async () => {
-		const readme = await getReadme();
+const getFileContents = async (path: string) => {
+	//read content from ./testData
+	const readme = await fs.readFile(path, 'utf8');
+	return {
+		content: readme,
+	};
+};
+
+const updateFileContents = async (path: string, newContent: string) => {
+	//write content to ./testData
+	await fs.writeFile(path, newContent);
+};
+
+describe('Tests with mock functions', () => {
+	test('Readme', async () => {
+		const paths = {
+			readme: './__tests__/readme-test/README.md',
+			expected: './__tests__/readme-test/README_EXPECTED.md',
+			new: './__tests__/readme-test/README_NEW.md',
+			todo: './__tests__/readme-test/TODO',
+		};
+
+		const readme = await getFileContents(paths.readme);
 		if (!readme) throw new Error('No readme found');
 
 		const segment = getSegment(readme.content);
 		if (segment === null) throw new Error('No segment found');
 
-		const todo = await getTodo();
+		const todo = await getFileContents(paths.todo);
 		if (!todo) throw new Error('No todo found');
 
 		const newSegment = createSegment(todo.content);
 		if (newSegment === segment) return; //no changes
 
 		const newReadme = updateSegment(readme.content, newSegment);
-		await updateReadme(newReadme);
+		await updateFileContents(paths.new, newReadme);
 
-		const expectedReadme = await fs.readFile('./__tests__/testData/README_EXPECTED.md', 'utf8');
-		const expectedSegment = getSegment(expectedReadme);
+		const expectedReadme = getFileContents(paths.expected);
+		const expectedSegment = getSegment((await expectedReadme).content);
 
 		const areEqual = expectedSegment === newSegment;
 		expect(areEqual).toBe(true);
 	});
-
-	const getReadme = async () => {
-		//read content from ./testData
-		const readme = await fs.readFile('./__tests__/testData/README.md', 'utf8');
-		return {
-			content: readme,
-		};
-	};
-
-	const getTodo = async () => {
-		//read content from ./testData
-		const todo = await fs.readFile('./__tests__/testData/TODO', 'utf8');
-		return {
-			content: todo,
-		};
-	};
-
-	const updateReadme = async (newReadme: string) => {
-		//write content to ./testData
-		await fs.writeFile('./__tests__/testData/README_NEW.md', newReadme);
-	};
 });
-
-// shows how the runner will run a javascript action with env / stdout protocol
-// test('test runs', () => {
-//   process.env['INPUT_MILLISECONDS'] = '500'
-//   const np = process.execPath
-//   const ip = path.join(__dirname, '..', 'lib', 'main.js')
-//   const options: cp.ExecFileSyncOptions = {
-//     env: process.env
-//   }
-//   console.log(cp.execFileSync(np, [ip], options).toString())
-// })
